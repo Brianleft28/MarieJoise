cecebb3c7d06f87eb69a06373a3a3511b9bc0ca6
Mirta Susana Segura <6693@Ituzaingo.dns>
2023-11-23 13:09:31 -0300

    0.1

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const razas = [
     Str: 5, //Fuerza
     Int: 8, //Inteligencia
     Skill: 5, // Destreza
-    Agli: 5, // Agilidad
+    Agi: 5, // Agilidad
     Stealth: 5, // Sigilo
   },
   {
@@ -239,12 +239,29 @@ botonElegirRaza.addEventListener('click', () => {
   mostrarTablaRaza(personaje.raza)
 })  
 
-
-
+      // CONTINUAR JUEGO //
+//// comienza de la historia ////
+  //        MOMENTO 1       //
 let continuarJuego = document.getElementById('continuarJuego')
 continuarJuego.addEventListener('click', ()=> {
-mostrarMensaje(-'Hola','output')
+outputDiv = document.getElementById('output2');
+outputDiv.style.display = 'none'
+titulo.innerHTML = 'Despertaste'
+  mostrarMensaje(`¡Hola ${personaje.nombre}!. Menos mal despertaste.Mi nombre es Fulanito y te he estado cuidando. Que bien que despertaste! En serio, necesitamos cruzar ese rio y nada mejor que un ${personaje.raza.nombre} para hacerlo... Te sigo, en serio. ¡En serio!`, 'output')
+
+  // cuando volvemos a tocar ocultamos el botón, y abrimos el menu desplegable. 
+  continuarJuego.addEventListener('click', ()=>{ 
+    mostrarMensaje(`...necesitamos cruzar ese rio y nada mejor que un ${personaje.raza.nombre} para hacerlo... Te sigo, en serio. ¡En serio!`, 'output')
+    let choises = document.getElementById('choises__div')
+    choises.style.display = 'flex';
+    mostrarMensaje('Opción 1','ch1') // OP 1
+    mostrarMensaje('Opción 2','ch2') // OP 2
+    mostrarMensaje('Opción 3','ch3') // OP 3
+    mostrarMensaje('Opción 4','ch4') // OP 4
+  })
 })
+
+
 
 
 
